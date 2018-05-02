ed8bb3d304d5d2e2ed90725f3a4a04489efffd82
ailindoyle <caitlin.doyle@mobile-life.com>
2018-05-02 16:46:45 +0100

    navbar and top see my work button navigate to sections and smooth scroll

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,28 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyNavbar()};
 
 var navbar = document.getElementById("navbar");
-
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function stickyNavbar() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
     }
 }
+
+$("#home").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
+
+$("#navbar").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
